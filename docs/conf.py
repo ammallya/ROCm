081223e2f24e8d490cd04a95078b4deec6564423
Alex Xu <alex.xu@amd.com>
2025-02-06 17:21:07 -0500

    change theme back to rocm

--- docs/conf.py
+++ docs/conf.py
@@ -99,8 +99,8 @@ html_context = {}
 if os.environ.get("READTHEDOCS", "") == "True":
     html_context["READTHEDOCS"] = True
 
-# html_theme = "rocm_docs_theme"
-html_theme = "sphinx_rtd_theme"
+html_theme = "rocm_docs_theme"
+# html_theme = "sphinx_rtd_theme"
 html_theme_options = {"flavor": "rocm-docs-home"}
 
 html_static_path = ["sphinx/static"]
