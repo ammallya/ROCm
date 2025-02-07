296b1e5075abe8f9d406bf9efa584b576e876ca4
Alex Xu <alex.xu@amd.com>
2025-02-07 14:04:28 -0500

    adjust selector

--- docs/sphinx/static/js/search.js
+++ docs/sphinx/static/js/search.js
@@ -1,6 +1,6 @@
 // Trigger the Read the Docs Addons Search modal when clicking on "Search docs" input from the topnav.
 // NOTE: The selector of the search input may need to be adjusted based on your theme.
-document.querySelector("button[aria-label='Search']").addEventListener("focusin", () => {
+document.querySelector(".search-button-field.search-button__button").addEventListener("focusin", () => {
     const event = new CustomEvent("readthedocs-search-show");
     document.dispatchEvent(event);
  });
